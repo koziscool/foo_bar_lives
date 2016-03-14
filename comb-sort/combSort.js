4c0d5df1790c2a4058b1d06d87807fc7469ecf80
koziscool <kozjob@yahoo.com>
2016-03-14 11:15:17 -0500

    first

--- comb-sort/combSort.js
+++ comb-sort/combSort.js
@@ -2,5 +2,20 @@
 
 var combSort = function(array) {
   // Your code here
+
+  // 1.3 is the gap shrink factor
+  var interval = Math.floor(array.length/1.3); ///  initializes the interval to roughly 3/4 of array length
+  while (interval > 0) {
+    for(var i=0; i+interval<array.length; i+=1) {  // this loop runs quasi-bubblesort
+      if (array[i] > array[i+interval]) {
+        var small = array[i+interval];  ////
+        array[i+interval] = array[i];   ////  swap two elements based on that are spaced by the current value of interval
+        array[i] = small;               ////
+      }
+    }
+    interval = Math.floor(interval/1.3);  // decreases the width of the interval, or the spacing between array elements being compared
+  }
+  return array;
+
 };
 
--- groceries/groceries.js
+++ groceries/groceries.js
@@ -1,2 +1,43 @@
 // Your Code Here!
 
+
+var myGroceries = [];
+
+var renderGroceries = function() {
+  $groceries = $('.show-groceries');
+  if( myGroceries.length === 0) {
+    $groceries.html('No groceries')
+  } else {
+    var htmlString = '<ul>'
+    myGroceries.each( function(el){
+      htmlString = htmlString + '<li>' + el.toString() + '</li>';
+    });
+    htmlString = htmlString + '</ul>';
+    $groceries.html(htmlString);
+  }
+}
+
+$(document).ready(function() {
+
+  $('.submit-new-grocery').click( function(event) {
+    var inputText = $('.add-grocery').val();
+    console.log(inputText);
+    if( inputText !== '') {
+      myGroceries.push( inputText );
+    }
+
+    renderGroceries();
+  });
+
+  $('.delete-grocery').click( function(event) {
+  });
+
+  $('.show-groceries li').on('click', function(event) {
+    var deleteText = $(this).val();
+    myGroceries.splice( myGroceries.indexOf(deleteText), 1 );
+    renderGroceries();
+  });
+
+
+});
+
